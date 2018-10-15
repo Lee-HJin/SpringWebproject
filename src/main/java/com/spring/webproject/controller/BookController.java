853ab2e4c63ff92d161eadeff450e1c4b3d907f2
jaehoonSong <luckysong777@gmail.com>
2018-10-15 18:18:55 +0900

    jaehoon_MainExample_1015

---
+++ src/main/java/com/spring/webproject/controller/BookController.java
@@ -0,0 +1,14 @@
+package com.spring.webproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BookController {
+
+	@RequestMapping(value="/book_novel.action", method= {RequestMethod.GET,RequestMethod.POST})
+	public String book_novel() {
+		return "/books/book_novel";
+	}
+}
