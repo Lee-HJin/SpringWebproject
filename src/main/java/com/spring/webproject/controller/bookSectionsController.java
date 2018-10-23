c648e60faf1454ca7934a828575e87db31324ff6
Jun <fkfmeod@hotmail.com>
2018-10-23 15:21:34 +0900

    dao, dto 작성 하기전 마무리 수정

--- src/main/java/com/spring/webproject/controller/bookSectionsController.java
+++ src/main/java/com/spring/webproject/controller/bookSectionsController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class bnlContoller {
+public class bookSectionsController {
 
 	@RequestMapping(value="/bnlBSList.action", method= {RequestMethod.GET, RequestMethod.POST})
 	public String list(HttpServletRequest request, HttpServletResponse response) throws Exception {
